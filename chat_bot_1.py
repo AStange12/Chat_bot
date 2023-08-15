4c3a20047ee159cbdd656248f33d16e7c3b1f9a5
Aaron Stange <aaron.w.stange@gmail.com>
2023-08-15 15:35:44 -0400

    allow cors

--- chat_bot_1.py
+++ chat_bot_1.py
@@ -8,6 +8,14 @@ class ChatBotHandler(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
+        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')  # Allow POST and OPTIONS requests
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+        
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
     def do_POST(self):
