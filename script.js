fa4ce2d562fb9f61afab30f714928a4745526912
Aaron Stange <aaron.w.stange@gmail.com>
2023-08-15 11:43:55 -0400

    add js and edited css and html file

---
+++ script.js
@@ -0,0 +1,38 @@
+const chatBox = document.getElementById('chat-box');
+const userInput = document.getElementById('user-input');
+const sendButton = document.getElementById('send-button');
+
+function displayUserMessage(message) {
+    const userMessage = document.createElement('div');
+    userMessage.classList.add('chat-message');
+    userMessage.textContent = message;
+    chatBox.appendChild(userMessage);
+}
+
+function displayBotMessage(message) {
+    const botMessage = document.createElement('div');
+    botMessage.classList.add('chat-message', 'bot-message');
+    botMessage.textContent = message;
+    chatBox.appendChild(botMessage);
+}
+
+sendButton.addEventListener('click', async () => {
+    const message = userInput.value;
+    if (message.trim() === '') return;
+
+    displayUserMessage(message);
+    userInput.value = '';
+
+    const response = await fetch('http://localhost:8000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+    });
+
+    const responseData = await response.json();
+    const botResponse = responseData.bot_response;
+
+    displayBotMessage(botResponse);
+});
