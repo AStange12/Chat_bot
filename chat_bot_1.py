e047309c47c716839751c1aceeaab882d25ea35e
Aaron Stange <aaron.w.stange@gmail.com>
2023-08-15 14:44:30 -0400

    added node

--- chat_bot_1.py
+++ chat_bot_1.py
@@ -1,89 +1,71 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
+import sys
 import re
 import long_responses as long
 
-class ChatBotHandler(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
+def get_bot_response(self, user_input):
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        user_message = json.loads(post_data)['message']
-        
-        bot_response = self.get_bot_response(user_message)
-        
-        self._set_response()
-        response = {'bot_response': bot_response}
-        self.wfile.write(json.dumps(response).encode('utf-8'))
+    def msg_prob(msg, known_words, single_resp=False, req_words=[], only_one=False):
+        msg_certainty = 0
+        has_req_words = True
 
-    def get_bot_response(self, user_input):
+        # Counts the amount of known words in the msg
+        for word in msg:
+            if word in known_words:
+                msg_certainty += 1
 
-        def msg_prob(msg, known_words, single_resp=False, req_words=[], only_one=False):
-            msg_certainty = 0
-            has_req_words = True
+        # Calcs the % of known words in msg
+        percentage = float(msg_certainty) / float(len(known_words))
 
-            # Counts the amount of known words in the msg
-            for word in msg:
-                if word in known_words:
-                    msg_certainty += 1
-
-            # Calcs the % of known words in msg
-            percentage = float(msg_certainty) / float(len(known_words))
-
-            # makes sure msg has req words
-            for word in req_words:
-                if only_one:
-                    if word in msg:
-                        break
-                else:
-                    if word not in msg:
-                        has_req_words = False
-                        break
-
-            # return statement  NOTE: make it so if it has one of a required word it will still return
-            if has_req_words or single_resp:
-                return int(percentage*100)
+        # makes sure msg has req words
+        for word in req_words:
+            if only_one:
+                if word in msg:
+                    break
             else:
-                return 0
+                if word not in msg:
+                    has_req_words = False
+                    break
 
-        def check_all_msgs(msg):
-            highest_prob_list = {}
+        # return statement  NOTE: make it so if it has one of a required word it will still return
+        if has_req_words or single_resp:
+            return int(percentage*100)
+        else:
+            return 0
 
-            def response(bot_resp, list_of_words, single_resp=False, req_words=[], only_one=False):
-                nonlocal highest_prob_list
-                highest_prob_list[bot_resp] = msg_prob(msg, list_of_words, single_resp, req_words, only_one)
+    def check_all_msgs(msg):
+        highest_prob_list = {}
 
-            # Responses ------------------------------------------------------------------------
-            response('Whats good!', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_resp=True)
-            response('robo tings', ['whats', 'up', 'what', 'are', 'you', 'doing'], req_words=['whats', 'doing'], only_one=True)
-            response('Good morn to you sir', ['good', 'morning'], req_words=['morning'])
-            response('good night, sleep tight', ['good', 'night', 'evening'], req_words=['night', 'evening'], only_one=True)
-            response(long.R_ROBOT, ['are', 'you', 'robot'], req_words=['you', 'robot'])
-            response('what', ['what', 'huh'], single_resp=True)
-            response('yuh huh', ['no', 'nah', 'nope'], single_resp=True)
-            response('nuh uh', ['yes', 'yep', 'yah'], single_resp=True)
-            response('I\'m chillin', ['how', 'are', 'you', 'doing'], req_words=['how'])
-            response('Thanks bruda', ['i', 'like', 'love', 'you'], req_words=['you'])
-            response(long.R_EATING, ['what', 'you', 'eat'], req_words=['you', 'eat'])
+        def response(bot_resp, list_of_words, single_resp=False, req_words=[], only_one=False):
+            nonlocal highest_prob_list
+            highest_prob_list[bot_resp] = msg_prob(msg, list_of_words, single_resp, req_words, only_one)
 
-            best_match = max(highest_prob_list, key=highest_prob_list.get)
-            # print(highest_prob_list)
+        # Responses ------------------------------------------------------------------------
+        response('Whats good!', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_resp=True)
+        response('robo tings', ['whats', 'up', 'what', 'are', 'you', 'doing'], req_words=['whats', 'doing'], only_one=True)
+        response('Good morn to you sir', ['good', 'morning'], req_words=['morning'])
+        response('good night, sleep tight', ['good', 'night', 'evening'], req_words=['night', 'evening'], only_one=True)
+        response(long.R_ROBOT, ['are', 'you', 'robot'], req_words=['you', 'robot'])
+        response('what', ['what', 'huh'], single_resp=True)
+        response('yuh huh', ['no', 'nah', 'nope'], single_resp=True)
+        response('nuh uh', ['yes', 'yep', 'yah'], single_resp=True)
+        response('I\'m chillin', ['how', 'are', 'you', 'doing'], req_words=['how'])
+        response('Thanks bruda', ['i', 'like', 'love', 'you'], req_words=['you'])
+        response(long.R_EATING, ['what', 'you', 'eat'], req_words=['you', 'eat'])
 
-            return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+        best_match = max(highest_prob_list, key=highest_prob_list.get)
+        # print(highest_prob_list)
 
-        split_msg = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
-        response = self.check_all_msgs(split_msg)
-        return response
+        return long.unknown() if highest_prob_list[best_match] < 1 else best_match
 
-def run(server_class=HTTPServer, handler_class=ChatBotHandler, port=8000):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Starting server on port {port}...')
-    httpd.serve_forever()
+    split_msg = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = self.check_all_msgs(split_msg)
+    return response
 
 if __name__ == '__main__':
-    run()
+    if len(sys.argv) != 2:
+        print("Usage: chat_bot_1.py <user_input>")
+        sys.exit(1)
+
+    user_input = sys.argv[1]
+    bot_response = get_bot_response(user_input)
+    print(bot_response)
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const http = require('http');
+const { spawn } = require('child_process');
+
 const chatBox = document.getElementById('chat-box');
 const userInput = document.getElementById('user-input');
 const sendButton = document.getElementById('send-button');
@@ -16,23 +19,26 @@ function displayBotMessage(message) {
     chatBox.appendChild(botMessage);
 }
 
-sendButton.addEventListener('click', async () => {
+sendButton.addEventListener('click', () => {
     const message = userInput.value;
     if (message.trim() === '') return;
 
     displayUserMessage(message);
     userInput.value = '';
 
-    const response = await fetch('http://localhost:8000', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ message }),
+    // Run the Python script using Node.js child_process
+    const pythonProcess = spawn('python', ['chat_bot_1.py', message]);
+
+    pythonProcess.stdout.on('data', (data) => {
+        const botResponse = data.toString().trim();
+        displayBotMessage(botResponse);
     });
 
-    const responseData = await response.json();
-    const botResponse = responseData.bot_response;
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Python Error: ${data}`);
+    });
 
-    displayBotMessage(botResponse);
+    pythonProcess.on('close', (code) => {
+        console.log(`Python process exited with code ${code}`);
+    });
 });
